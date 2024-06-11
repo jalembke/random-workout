0a9e1eb3b9505db10262803a468e40c4acbdbefa
lembke <lembke@msoe.edu>
2024-06-11 18:49:38 -0500

    Initial Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-const App = () => {
-    return (
-        <div>Hi</div>
-    )
-};
+import lifts from "./lifts.json";
+
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+}
+
+const getRoutine = (number) => (
+    Object.keys(lifts).map(type => (
+        <h2 key={`${type}${number}`}>{type} - {lifts[type][getRandomInt(lifts[type].length)]}</h2>
+    ))
+)
+
+const App = () => (
+    <>
+        {getRoutine(1)}
+        {getRoutine(2)}
+    </>
+);
 
 export default App;
